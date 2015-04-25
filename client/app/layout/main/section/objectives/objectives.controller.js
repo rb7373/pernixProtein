7c86a7eb523d2985942eb3eac09301621164edf2
ruben abarca <abarca.ruben73@gmail.com>
2015-04-25 02:42:26 -0600

    responsive objectives section

--- client/app/layout/main/section/objectives/objectives.controller.js
+++ client/app/layout/main/section/objectives/objectives.controller.js
@@ -5,10 +5,10 @@
     .module('proteinApp')
     .controller('ObjectivesController', ObjectivesController);
 
-  ObjectivesController.$inject = ['navigationService', '$location'];
+  ObjectivesController.$inject = ['navigationService', '$location', '$mdMedia', '$scope'];
 
   /* @ngInject */
-  function ObjectivesController(navigationService, $location) {
+  function ObjectivesController(navigationService, $location, $mdMedia, $scope) {
     /* jshint validthis: true */
     var vm = this;
 
@@ -26,11 +26,18 @@
 
     function activate() {
       var section = vm.navigation.getCurrentSectionNumber();
+
       if (section === 0) {
         $location.path('/');
       }
 
+      vm.screenIsSmall = $mdMedia('sm');
+
     }
+
+    $scope.$watch(function() { return $mdMedia('sm'); }, function(sm) {
+      vm.screenIsSmall = sm;
+    });
 
 
   }
